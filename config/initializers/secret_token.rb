b756bb5c8751e984390f17862fb611867fc775ec
binary10ve <sanil.m.naik@gmail.com>
2014-02-22 17:53:29 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrewIdeas::Application.config.secret_token = '26bb33653598c37a0d7f9f707310a677c4a7e7691412c2562f4094ad5540998311cfcdb3aefca6ae7298f0bf229d5f59c87f6cb6df5db476ffa6bc3003404683'
