6c3aa39cb6463b2214601d8529e91c940fa0f69e
Krunal <krunal.shah@hotmail.com>
2014-03-25 17:00:57 -0500

    Added facebook and google login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,42 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
+
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid
   # attr_accessible :title, :body
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+    data = access_token.info
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+    if user
+      return user
+    else
+	    registered_user = User.where(:email => access_token.info.email).first
+	    if registered_user
+	    	return registered_user
+	  	else
+		    user = User.create(provider:access_token.provider,
+		      email: data["email"],
+		      uid: access_token.uid ,
+		      password: Devise.friendly_token[0,20],
+		    )
+	  	end
+    end
+  end
+
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      user = User.create(  provider:auth.provider,
+                           uid:auth.uid,
+                           email:auth.info.email,
+                           password:Devise.friendly_token[0,20]
+                           )
+      #user.ensure_authentication_token!
+      # added extra to create authentication token for user
+    end
+    user
+  end
 end
