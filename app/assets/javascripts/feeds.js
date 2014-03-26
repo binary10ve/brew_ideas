c062ad438753224d5d68dfa5558adc0729930984
binary10ve <sanil.m.naik@gmail.com>
2014-03-26 23:44:44 +0530

    turbolinks added

--- app/assets/javascripts/feeds.js
+++ app/assets/javascripts/feeds.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+function FeedsView(container){
+
+	this.container = container;
+	this.init();
+
+};
+
+FeedsView.prototye.init = function(){
+	this.attachEvents();
+};
+
+FeedsView.prototye.attachEvents = function(){
+	
+};
+
+
+FeedsView.prototye.destroy = function(){
+	
+};
