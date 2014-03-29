6491c8c6b7369bf1a34ee585d7a2d557150c8474
Krunal <krunal.shah@hotmail.com>
2014-03-29 15:17:20 -0500

    Changes for feed creation

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,11 @@
 class Feed < ActiveRecord::Base
   # attr_accessible :title, :body
+  attr_accessible :user_id, :idea_id
   belongs_to :idea
   has_one :feed_like
   has_one :feed_dislike
+
+  def self.create_feed(options)
+  	Feed.create!(user_id: options[:idea].user_id)
+  end
 end
