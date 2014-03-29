73e114e14c3ad5e18e2ab1fc5c29ce29b0c21fba
Krunal <krunal.shah@hotmail.com>
2014-03-29 15:17:51 -0500

    Changes for feed creation

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,11 +1,22 @@
 class Idea < ActiveRecord::Base
-  attr_accessible :description, :title
+  attr_accessible :description, :title, :feed_id, :user_id
   scope :popular
   scope :mine
   scope :recent, :order => "created_at desc"
   scope :search  
-   scope :search, ->(query) { where("title like '%#{query}%' or description like '%#{query}%'") }
+  scope :search, ->(query) { where("title like '%#{query}%' or description like '%#{query}%'") }
 
   has_one :feed
   has_many :tags , through: :idea_tags
+
+  after_create :create_feed
+ 
+
+
+  def create_feed
+  	feed = Feed.create_feed({idea: self})
+  	self.feed_id = feed.id
+  	self.save!
+  end
+
 end
